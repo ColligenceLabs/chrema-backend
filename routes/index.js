24cdcb02f4832bba9ba9c718cb49ce54727189f9
devteam@colligence.io <devteam@colligence.io>
2021-11-10 20:20:33 +0900

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const {USER_TABLE} = require('../utils/consts');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/backend', function (req, res, next) {
     res.render('index', {title: 'Home'});
 });
 
