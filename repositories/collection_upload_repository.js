8aa470a5c739b0caac6e07f26007832dd84a26d9
정용인 <lordofbabo@colligence.io>
2021-12-03 15:16:56 +0900

    add new file

---
+++ repositories/collection_upload_repository.js
@@ -0,0 +1,23 @@
+const util = require('util');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, process.cwd() + '/uploads/cover/');
+    },
+    filename: (req, file, cb) => {
+        var uniqueSuffix = Date.now() + '-';
+        cb(null, uniqueSuffix + file.originalname);
+    },
+});
+
+// let uploadFile = multer({
+//     storage: storage,
+// }).single('file');
+let uploadFile = multer({
+    storage: storage,
+}).fields([{name: 'file'}, {name: 'thumbnail'}]);
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+
+module.exports = uploadFileMiddleware;
