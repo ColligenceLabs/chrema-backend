fd54dac9c195393503f2c65c06cc701749c4fc90
정용인 <lordofbabo@colligence.io>
2021-12-03 19:01:34 +0900

    data to json

--- controllers/webview/nft_controller.js
+++ controllers/webview/nft_controller.js
@@ -189,10 +189,10 @@ module.exports = {
             }
             let data = {_id: req.params.id};
             let nft = await nftRepository.findQuantitySelling(data);
-            nft = JSON.parse(JSON.stringify(nft));
             if (!nft) {
                 return handlerError(req, res, ErrorMessage.NFT_IS_NOT_FOUND);
             }
+            nft = JSON.parse(JSON.stringify(nft));
 
             return handlerSuccess(req, res, nft);
         } catch (error) {
