1a8f93ae7dc92b8c6ee54b14c53b5a765f4d2f0a
devteam@colligence.io <devteam@colligence.io>
2021-12-12 21:48:23 +0900

    test

--- controllers/blockchain/nft_controller.js
+++ controllers/blockchain/nft_controller.js
@@ -6,13 +6,12 @@ const chainId = process.env.KLAYTN_CHAIN_ID | 0;
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const contractAddress = process.env.NFT_CONTRACT_ADDR;
-const option = {
-    headers: [
+const option = [
         // { name: 'Authorization', value: 'Basic ' + Buffer.from(accessKeyId + ':' + secretAccessKey).toString('base64') },
         // { name: 'x-chain-id', value: chainId },
         { name: "x-krn", value:"krn:8217:wallet:ebf2bc57-9c0f-48f7-afc5-e260cbb57ec3:account-pool:NFTMarketplace" },
     ]
-}
+
 
 const caver = new CaverExtKAS(chainId, accessKeyId, secretAccessKey, option);
 
