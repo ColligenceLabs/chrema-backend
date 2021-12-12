21e0a7524f6d9c40ff6085f23e02346d1d6b750b
정용인 <lordofbabo@colligence.io>
2021-12-12 21:57:12 +0900

    fix error

--- controllers/webview/user_controller.js
+++ controllers/webview/user_controller.js
@@ -422,7 +422,7 @@ module.exports = {
             let findParams = {
                 buyer: user._id,
                 // status: consts.TRANSACTION_STATUS.PROCESSING,
-                {$or: [{status: consts.TRANSACTION_STATUS.PROCESSING},{status: consts.TRANSACTION_STATUS.ERROR}]}
+                $or: [{status: consts.TRANSACTION_STATUS.PROCESSING},{status: consts.TRANSACTION_STATUS.ERROR}]
             };
             let transactions = await txRepository.findTxs(findParams, {page, perPage});
 
