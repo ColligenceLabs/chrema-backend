608dab176dbabbffb2f0ef74594688f2df9f747b
Peter Nahm <hwnahm@gmail.com>
2022-02-04 20:40:40 +0900

    express url encoding test

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
