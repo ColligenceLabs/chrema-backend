55f2001d9dcbda1e7125791eb1b9ff2cbb55c2cd
Peter H. Namh <hwnahm@gmail.com>
2022-03-17 15:50:17 +0900

    add crator name and image to collections list

--- repositories/collection_repository.js
+++ repositories/collection_repository.js
@@ -31,7 +31,7 @@ module.exports = {
                 .skip((pagination.page - 1) * pagination.perPage)
                 .limit(pagination.perPage)
                 .sort({createdAt: -1, _id: 1})
-                .populate({path: 'creator_id', select: 'name'});
+                .populate({path: 'creator_id', select: ['full_name', 'image']});
             return collections;
         } catch (error) {
             return error;
