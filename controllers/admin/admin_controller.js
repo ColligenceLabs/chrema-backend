f886dbb995d46b687827f4a2dfc12db30fadb854
Peter H. Namh <hwnahm@gmail.com>
2022-05-17 09:39:10 +0900

    fix: updateAadmin can change description

--- controllers/admin/admin_controller.js
+++ controllers/admin/admin_controller.js
@@ -435,5 +435,9 @@ async function getUpdateBodys(updates) {
         updateBodys.status = updates.status;
     }
 
+    if (updates.description) {
+        updateBodys.description = updates.description;
+    }
+
     return {updateBodys: updateBodys, errors: errors};
 }
