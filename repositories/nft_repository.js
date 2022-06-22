f43bd867562a8c6d608589e82460547efa7d8090
Peter H. Namh <hwnahm@gmail.com>
2022-06-22 11:31:07 +0900

    fix: increase batchSize

--- repositories/nft_repository.js
+++ repositories/nft_repository.js
@@ -144,7 +144,7 @@ module.exports = {
     },
 
     findAllNftsByCollectionId: async function (id) {
-        let nfts = await NftModel.find({collection_id: id}).batchSize(100000);
+        let nfts = await NftModel.find({collection_id: id}).batchSize(500000);
         if (!nfts) {
             return null;
         }
