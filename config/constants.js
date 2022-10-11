24c5a13105611e36edbeb15a358ce38d611a1412
Peter H. Namh <hwnahm@gmail.com>
2022-10-11 19:20:22 +0900

    feat: update market contract

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@ module.exports = {
         3: '',
         4: '',
         8217: '0xd5ed88ccC09a43d884618C5E3ee1935ad8a8ca93',
-        1001: '0xe8abbee2e5dd5e4ef5791166ff67c6aea4ecee69', // '0xB590C13Ab0EcE4951c235b1Bc8bBe25C54c1261b',
+        1001: '0x6cef313442481a9c29eb5784bd01e669eab25674', // '0xB590C13Ab0EcE4951c235b1Bc8bBe25C54c1261b',
         56: '0x81b2Fc99c5EA3C062D7Eb147E5fd9e331aCcC72b',
         97: '0x7dcA6d76dD8f8c72229111C2e8999F6Dd7452d49'
     },
