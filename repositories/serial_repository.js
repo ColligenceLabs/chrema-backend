a7b3913ecfd815a720631414a592e7e3286e54fa
Peter H. Namh <hwnahm@gmail.com>
2022-10-11 18:39:05 +0900

    feat: support rental nft as 1155

--- repositories/serial_repository.js
+++ repositories/serial_repository.js
@@ -101,7 +101,8 @@ module.exports = {
     findByOwnerIdAndNftId: async function (ownerId, nftId) {
         try {
             let serials = await SerialModel.find({owner_id: ownerId, nft_id: nftId})
-                .sort({index: 1, _id: 1});
+                .sort({index: 1, _id: 1})
+                .populate({path: 'nft_id', populate: {path: 'creator_id'}});
             // .populate({path: 'owner_id', select: 'uid'});
             return serials;
         } catch (error) {
